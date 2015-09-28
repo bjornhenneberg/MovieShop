f7ceeffc0cdb5ef02c8f6ae46317e13c2d421c87
h34dl3zz <bjorniz@hotmail.dk>
2015-09-28 10:54:28 +0200

    order crud

---
+++ Movieshop/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MoviesShopProxy.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movieshop.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public OrderViewModel()
+        {
+
+        }
+
+        public Order order { get; set; }
+        public Customer customer { get; set; }
+        public Movie movie { get; set; }
+
+    }
+}
