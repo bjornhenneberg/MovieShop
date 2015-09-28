544c5f2439909233b4cc9544383f8c281304f441
Søren <srhrandersen@gmail.com>
2015-09-28 12:42:54 +0200

    Customer index view are now working with adress

---
+++ Movieshop/Models/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MoviesShopProxy.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movieshop.Models.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public List<Customer> customers { get; set; }
+        public List<Adress> adresses { get; set; }
+    }
+}
