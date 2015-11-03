7279004ae4d837c28afff8f707500641de032454
Jonathan <jonathan@sighotel.dk>
2015-11-03 12:10:15 +0100

    Maybe save for backup

---
+++ MovieShopRest/MovieShopDTO/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieShopDTO
+{
+    public class Class1
+    {
+    }
+}
---
+++ MovieShopRest/MovieShopRest/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using MovieShopDAL;
+using MovieShopDAL.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace MovieShopRest.Controllers
+{
+    public class MovieController : ApiController
+    {
+        private Facade facade = new Facade();
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new Facade().GetMovieRepository().ReadAll();
+        }
+
+    }
+}
